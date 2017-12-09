86733c7a26d1c5b4d7310c97e53e8afe0f3f50cb
Unknown <linuxfarid@gmail.com>
2017-12-09 20:50:02 +0100

    Connection to eureka server

--- src/main/java/dz/ava/CurrencyConversionServiceApplication.java
+++ src/main/java/dz/ava/CurrencyConversionServiceApplication.java
@@ -2,10 +2,13 @@ package dz.ava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-@EnableFeignClients
+
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
