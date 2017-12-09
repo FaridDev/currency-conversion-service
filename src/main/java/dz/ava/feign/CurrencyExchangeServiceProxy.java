a5898af2bd17787a74857198fafdc796f1ee16b1
Unknown <linuxfarid@gmail.com>
2017-12-09 20:25:04 +0100

    Add Ribbon Load Balancing

--- src/main/java/dz/ava/feign/CurrencyExchangeServiceProxy.java
+++ src/main/java/dz/ava/feign/CurrencyExchangeServiceProxy.java
@@ -2,10 +2,13 @@ package dz.ava.feign;
 
 import dz.ava.bean.CurrencyConversionBean;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url = "http://localhost:8000")
+//@FeignClient(name = "currency-exchange-service", url = "http://localhost:8000")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
